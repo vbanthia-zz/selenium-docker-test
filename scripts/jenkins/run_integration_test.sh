7aedea7786dd6736c9ffe60094f746a89e5a85e4
Vishal Banthia <vishal.banthia.vb@gmail.com>
2015-10-27 19:28:49 +0900

    feature type

--- scripts/jenkins/run_integration_test.sh
+++ scripts/jenkins/run_integration_test.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 set -e
 
-while getopts t: OPT
-do
-        case $OPT in
-                "t" ) TARGET_TEST="$OPTARG" ;;
-                  * ) echo "Usage: $CMDNAME [-t TARGET_TEST]" 1>&2; exit 1 ;;
-        esac
-done
+if [ "$FEATURE_TYPE" == "" ]; then
+  echo "please set FEATURE_TYPE, otherwise test will not run."
+  exit 1
+fi
 
-#
-# if [ "$TARGET_TEST" == "" ]; then
-#   echo "please set TARGET_TEST, otherwise test will not run."
-#   exit 1
-# fi
-
-bundle exec rake spec:$TARGET_TEST
+bundle exec rake spec:$FEATURE_TYPE
